ea5af778c61869ba51504fdf35db37a31718d2f1
Andrew Greenman <agreenman@Andrews-MacBook-Pro.local>
2023-05-03 11:30:01 -0400

    Initial Commit

---
+++ Hunter/HunterApp.swift
@@ -0,0 +1,17 @@
+//
+//  HunterApp.swift
+//  Hunter
+//
+//  Created by Andrew Greenman on 5/3/23.
+//
+
+import SwiftUI
+
+@main
+struct HunterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
